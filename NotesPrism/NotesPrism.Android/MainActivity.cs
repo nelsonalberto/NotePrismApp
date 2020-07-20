420e2394f31a25e3baf8be61044e1d9692b29e47
Nelson José Alberto <nelsonjose.alberto@hotmail.com>
2020-07-20 15:22:10 -0600

    Adding splash screen and deleting bad files

--- NotesPrism/NotesPrism.Android/MainActivity.cs
+++ NotesPrism/NotesPrism.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Prism.Ioc;
 
 namespace NotesPrism.Droid
 {
-    [Activity(Label = "NotesPrism", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "NotesPrism", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
